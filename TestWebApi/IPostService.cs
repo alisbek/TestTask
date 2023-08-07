fa53c53677043dfac3aecefec22978e217ae740e
AlisAlymkulov <61144938+AlisAlymkulov@users.noreply.github.com>
2023-08-07 14:52:00 +0200

    cleann

--- TestWebApi/IPostService.cs
+++
@@ -1,7 +0,0 @@
-namespace PostEngine.Controllers;
-
-public interface IPostService
-{
-    IEnumerable<Post> GetLatestUserPosts(int userId, int maxCount);
-    IEnumerable<Post> GetLatestPostsFromMultipleUsers(int maxCount);
-}
--- TestWebApi/Post.cs
+++
@@ -1,9 +0,0 @@
-namespace PostEngine.Controllers;
-
-public class Post
-{
-    public int Id { get; set; }
-    public int UserId { get; set; }
-    public DateTime PublishDate { get; set; }
-    public string Message { get; set; }
-}
